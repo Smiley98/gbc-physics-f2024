9ae90f0db0b0162693e4c21ab93e080920d14ff8
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-11 14:32:20 -0400

    Added homework comments

--- Assets/Scripts/Bounce.cs
+++ Assets/Scripts/Bounce.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
-    float vel = 0.0f;
+    // Consider using 3d vectors instead of 1d scalars to describe velocity & acceleration
+    public float acc = Physics.gravity.y;
+    public float vel = 0.0f;
+    public float pos = 5.0f;
+    // Acceleration is "change in velocity"
+    // Velocity is "change in position"
 
     void FixedUpdate()
     {
         float dt = Time.fixedDeltaTime;
-        float acc = Physics.gravity.y;
+        vel = vel + acc * dt; // Update velocity based on acceleration
+        pos = pos + vel * dt; // Update position based on velocity
 
-        vel = vel + acc * dt;
+        // Apply motion to Unity GameObject via Transform component
+        transform.position = new Vector3(0.0f, pos, 0.0f);
 
-        transform.position = transform.position + new Vector3(0.0f, vel * dt, 0.0f);
+        // Homework:
+        // if ball is under the ground:
+        //      -set ball position to initial height
+        //      -set ball velocity to zero
     }
 }
