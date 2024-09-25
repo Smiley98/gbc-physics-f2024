fee064a421506bc8feab9325da0f38ec8124f87d
Smiley98 <smileysofficialcinema@gmail.com>
2024-09-25 14:44:43 -0400

    Made simpler fixed-step implementation

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -9,17 +9,23 @@ public class Game : MonoBehaviour
         
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        // The following loop ensures we only *ever* update (step) our physics system
-        // at the desired frequency (100hz). 
-        float dt = Time.deltaTime;
-        physicsSystem.frameTime += dt;
-        if (physicsSystem.frameTime >= physicsSystem.stepFrequency)
-        {
-            physicsSystem.Step(physicsSystem.stepFrequency);
-            float extraTime = physicsSystem.frameTime - physicsSystem.stepFrequency;
-            physicsSystem.frameTime = extraTime;
-        }
+        physicsSystem.Step(Time.fixedDeltaTime);
     }
+
+    // This is over-engineered. Consider rolling your own "fixed-update for homework"
+    //void Update()
+    //{
+    //    // The following loop ensures we only *ever* update (step) our physics system
+    //    // at the desired frequency (100hz). 
+    //    float dt = Time.deltaTime;
+    //    physicsSystem.frameTime += dt;
+    //    if (physicsSystem.frameTime >= physicsSystem.stepFrequency)
+    //    {
+    //        physicsSystem.Step(physicsSystem.stepFrequency);
+    //        float extraTime = physicsSystem.frameTime - physicsSystem.stepFrequency;
+    //        physicsSystem.frameTime = extraTime;
+    //    }
+    //}
 }
--- Assets/Scripts/PhysicsSystem.cs
+++ Assets/Scripts/PhysicsSystem.cs
@@ -5,12 +5,6 @@ public class PhysicsSystem
     // Default gravity of -9.81m/s^2 downwards (along the y-axis)
     public Vector3 gravity = new Vector3(0.0f, -9.81f, 0.0f);
 
-    // Default step frequency of 100 times per second
-    public float stepFrequency = 1.0f / 100.0f;
-
-    public float frameTime = 0.0f;
-
-
     public void Step(float dt)
     {
         Debug.Log("Desired update frequency: " + stepFrequency);
