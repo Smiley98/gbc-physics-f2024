e41524b083bc532a5caea34bb8c2d9a4d2284479
Smiley98 <smileysofficialcinema@gmail.com>
2024-10-02 15:31:19 -0400

    Added re-usable integration ("motion update") function

--- Assets/Scripts/PhysicsSystem.cs
+++ Assets/Scripts/PhysicsSystem.cs
@@ -13,29 +13,24 @@ public class PhysicsSystem
     // TODO -- add multiply bodies within Game's start function
     public List<PhysicsBody> bodies = new List<PhysicsBody>();
 
-    // Example of the behaviour I want *multiple* of:
     public PhysicsBody testBody;
 
     public void Step(float dt)
     {
-        // Time information:
-        //Debug.Log("Desired update frequency: " + Time.fixedDeltaTime);
-        //Debug.Log("Actual update frequency: " + dt);
-        //Debug.Log("Total time: " + time);
-
-        // TODO -- apply motion (acceleration, velocity, position) to list of bodies instead of just testBody
-
-        // Right now, our only acceleration is due to gravity,
         // so we'll store it as a uniform acceleration for all objects
         Vector3 acc = gravity;
 
         // Apply drag to velocity
         testBody.vel *= Mathf.Pow(testBody.drag, dt);
+        
+        // Apply motion
+        Integrate(ref testBody.vel, acc, dt);
+        Integrate(ref testBody.pos, testBody.vel, dt);
+    }
 
-        // "New velocity is equal to old velocity + change in velocity" (a * t)
-        testBody.vel = testBody.vel + acc * dt;
-
-        // "New position is equal to old position + change in position" (v * t)
-        testBody.pos = testBody.pos + testBody.vel * dt;
+    // "Integration" is updating a value based on the previous value + its change
+    private void Integrate(ref Vector3 value, Vector3 change, float dt)
+    {
+        value = value + change * dt;
     }
 }
